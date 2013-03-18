cdc6ff0b86593ee61f51b9cff5dec07a2583da44
Preston Timmons <prestontimmons@gmail.com>
2013-03-18 18:04:39 -0500

    More setup.py tweaks.

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
-with open("README.md") as f:
-    long_description = f.read()
+DESCRIPTION = """
+A more powerful Django include tag.
 
+* Works with recursive includes
+* Allows templates to be conditionally included, based on whether they exist
+* Allows individual blocks to be included from a template
+* Backwards-compatible with the Django built-in include tag
+
+See:
+
+https://github.com/prestontimmons/partial_include
+"""
 
 setup(
     name="partial-include",
@@ -10,7 +19,7 @@ setup(
     author="Preston Timmons",
     url="https://github.com/prestontimmons/partial_include",
     description="A more powerful include tag for Django",
-    long_description=long_description,
+    long_description=DESCRIPTION,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
